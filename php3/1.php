6b636d57cbafdd1364c8727f542551b6d9f0ded4
dylan <dylanspin100@hotmail.com>
2019-04-09 10:30:59 +0200

    begin php 3

---
+++ php3/1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Opdracht_1</h1><br>
+    <h2></h2><br>
+
+
+    <?php
+Class NewClass{
+
+public $info = "random informatie";
+}
+
+$object = new NewClass;
+var_dump($object);
+
+     ?>
+
+  </body>
+</html>
