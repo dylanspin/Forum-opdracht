a2a6372725e70ad26d430f1fd778a61fdab78360
dylan <dylanspin100@hotmail.com>
2019-04-09 15:42:51 +0200

    eind loa

---
+++ php3/2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h1>opdracht_5</h1>
+      <h2></h2>
+    <?php
+    Class UserClass{
+
+      public $voornaam = "dylan";
+      public $achternaam = "spin";
+      public $geboortedatum = "09-07-2002";
+      private $username = "een username";
+      private  $password = "een wachtwoord";
+    }
+     ?>
+
+  </body>
+</html>
