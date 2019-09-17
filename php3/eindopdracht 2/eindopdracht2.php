b087f8e8a11038094be554ccf28829c004189af9
dylanspin <47520114+dylanspin@users.noreply.github.com>
2019-09-17 16:44:06 +0200

    Update eindopdracht2.php

--- php3/eindopdracht 2/eindopdracht2.php
+++ php3/eindopdracht 2/eindopdracht2.php
@@ -22,15 +22,6 @@
 
   <div class="vierkant">
     <?php
-    //wat er moet gebeuren ik denk dat ik een array moet maken met 8 cijfers er in dan maak ik een table die 4 bij 4 is met een for loop
-    //dan als er twee zijn omgedraaid wat gebeurd met true en false statement als die true is dan echo die een andere kleur input en bij false
-    //een witte input als twee vakjes de zelfde value hebben dan moet dat vakje weg dan
-
-    //session als een sesion var true is dan moet die de vakjes echo als die false is dan niet of een andere kleur geven dus dat moet die checken met
-    //een if statement dan moeten de vakjes random getalen krijgen van 1 tot 8 en dan moet die 2 keer voor komen.
-    //dan kan je ze aan klikken als er 3 zijn aangelikt eigelijk 2 maar bij de 3 klick moeten alle vakjes weer wit worden
-    //als er 2 dezelfde getallen hebben dan moeten ze een andere kleur krijgen of weg gehaalt. als ze allemaal goed zijn moet er een knop komen
-    //als je daar op klikt dan word de game gereset
       include('include.php');
     ?>
 
